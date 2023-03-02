23249ed6dbb2044348c5fae5b02a415a912800ae
mattiavisona <mattiav2706@gmail.com>
2023-03-02 16:05:59 +0100

    exercised finished

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ ALTRIMENTI non può giocare
 */
 
 
-let textEmailElement = document.getElementById("textEmail")
-let buttonEmailElement = document.getElementById("buttonEmail")
+let textEmailElement = document.getElementById("textEmail");
+
+let buttonEmailElement = document.getElementById("buttonEmail");
+
+let welcomeOrNotElement = document.getElementById("welcomeornot");
 
 
 
@@ -39,13 +42,68 @@ buttonEmailElement.addEventListener("click", function () {
         
     }
 
-    // Se l'email è corretta do il benvenuto altrimenti no
+    // Se l'email è corretta do il benvenuto altrimenti do errore
 
     if (mailFound) {
-        console.log("Welcome");
+        
+        welcomeOrNotElement.innerText = "Welcome";
     
     } else {
-        console.log("Error");
+        
+        welcomeOrNotElement.innerText = "Error";
     }
 
 })
+
+// Inizio gioco dei dadi
+
+/*
+
+---- L'utente inizia il gioco e il computer gli genera un numero a caso da 1 a 6
+---- Viene generato un numero random al computer da 1 a 6
+? SE il numero dell'utente è maggiore del numero del computer
+--> Stampo hai vinto
+ALTRIMENTI
+--> Hai perso
+
+*/
+
+let diceFaceImg = ["dice-img/dice-face-1.jpeg", "dice-img/dice-face-2.webp", "dice-img/dice-face-3.png", "dice-img/dice-face-4.jpg", "dice-img/dice-face-5.jpg", "dice-img/dice-face-6.png"];
+
+const diceImg1Element = document.getElementById("diceImg1")
+
+const diceImg2Element = document.getElementById("diceImg2");
+
+// L'utente inizia il gioco e il computer gli genera un numero a caso da 1 a 6
+
+let generateUserNumberElement = Math.floor(Math.random() * 6 + 1);
+
+console.log(generateUserNumberElement);
+
+// Viene generato un numero random al computer da 1 a 6
+
+let generateComputerNumberElement = Math.floor(Math.random() * 6 + 1);
+
+console.log(generateComputerNumberElement);
+// ? SE il numero dell'utente è maggiore del numero del computer
+
+if (generateUserNumberElement > generateComputerNumberElement) {
+
+    console.log("Hai vinto");
+
+} else {
+    if (generateComputerNumberElement > generateUserNumberElement) {
+
+        console.log("hai perso");
+    }
+
+} if (generateComputerNumberElement == generateUserNumberElement) {
+    console.log("Pareggio");
+    
+}
+
+diceImg1Element.src = diceFaceImg [generateUserNumberElement - 1];
+
+diceImg2Element.src = diceFaceImg [generateComputerNumberElement - 1]
+
+
